e08368d6ea98b6b68d593f2863d6f4bec23d6d80
MattUDCompSci <mattweis@udel.edu>
2019-11-08 20:20:07 -0500

    Added Background and Sprite Images for First Screen

--- game_starter.py
+++ game_starter.py
@@ -2,17 +2,18 @@ import arcade
 
 
 # Define constants
-WINDOW_WIDTH = 500
+WINDOW_WIDTH = 600
 WINDOW_HEIGHT = 500
 BACKGROUND_COLOR = arcade.color.BLACK
-GAME_TITLE = "Introduction"
+GAME_TITLE = "Feed Zeke"
 GAME_SPEED = 1/60
 
-MATTHEWSCALING = 0.25
-TILE_SCALING = 0.05
+MATTHEWSCALING = 0.24
+TILE_SCALING = 0.4
 
-JUMPSPEED = 3
+JUMPSPEED = 15
 MOVESPEED = 5
+GRAVITY = 1
 
 class YourGameClassRenameThis(arcade.Window):
     def __init__(self):
@@ -32,25 +33,24 @@ class YourGameClassRenameThis(arcade.Window):
         self.coin_list = arcade.SpriteList()
         self.background = arcade.load_texture("images/Cowboys_Stadium.jpeg")
 
-        self.player_sprite = arcade.Sprite("images/ada.png", MATTHEWSCALING)
+        self.player_sprite = arcade.Sprite("images/Zeke_Right.png", MATTHEWSCALING)
         self.player_sprite.center_x = 64
-        self.player_sprite.center_y = 280
+        self.player_sprite.center_y = 150
         self.player_list.append(self.player_sprite)
 
         for x in range(0, 1250, 64):
-            wall = arcade.Sprite("images/potato.png", TILE_SCALING)
+            wall = arcade.Sprite("images/Football_turf.jpg", TILE_SCALING)
             wall.center_x = x
             wall.center_y = 32
             self.wall_list.append(wall)
 
-        self.physics_engine = arcade.PhysicsEngineSimple(self.player_sprite, self.wall_list)
+        self.physics_engine = arcade.PhysicsEnginePlatformer(self.player_sprite, self.wall_list, GRAVITY)
 
     def on_draw(self):
         """ Called when it is time to draw the world """
         arcade.start_render()
         arcade.draw_texture_rectangle(WINDOW_WIDTH // 2, WINDOW_HEIGHT // 2, WINDOW_WIDTH, WINDOW_HEIGHT,
                                       self.background)
-
         self.wall_list.draw()
         self.coin_list.draw()
         self.player_list.draw()
@@ -61,22 +61,17 @@ class YourGameClassRenameThis(arcade.Window):
 
     def on_key_press(self, key, modifiers):
         if key == arcade.key.UP or key == arcade.key.W:
-            self.player_sprite.change_y = JUMPSPEED
-        elif key == arcade.key.DOWN or key == arcade.key.S:
-            self.player_sprite.change_y = -JUMPSPEED
-        elif key == arcade.key.RIGHT or key == arcade.key.D:
-            self.player_sprite.change_x = MOVESPEED
+            if self.physics_engine.can_jump():
+                self.player_sprite.change_y = JUMPSPEED
         elif key == arcade.key.LEFT or key == arcade.key.A:
             self.player_sprite.change_x = -MOVESPEED
+        elif key == arcade.key.RIGHT or key == arcade.key.D:
+            self.player_sprite.change_x = MOVESPEED
 
     def on_key_release(self, key, modifiers):
-        if key == arcade.key.UP or key == arcade.key.W:
-            self.player_sprite.change_y = 0
-        elif key == arcade.key.DOWN or key == arcade.key.S:
-            self.player_sprite.change_y = 0
-        elif key == arcade.key.RIGHT or key == arcade.key.D:
+        if key == arcade.key.LEFT or key == arcade.key.A:
             self.player_sprite.change_x = 0
-        elif key == arcade.key.LEFT or key == arcade.key.A:
+        elif key == arcade.key.RIGHT or key == arcade.key.D:
             self.player_sprite.change_x = 0
 
 
