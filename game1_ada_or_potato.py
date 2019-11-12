b31cef28eef6b11d6d6179c013c5e7840d2592cb
MattUDCompSci <mattweis@udel.edu>
2019-11-12 11:44:45 -0500

    Finished Ada and Potato Code

--- game1_ada_or_potato.py
+++ game1_ada_or_potato.py
@@ -1,5 +1,4 @@
 import arcade
-import random
 
 WINDOW_WIDTH = 500
 WINDOW_HEIGHT = 500
@@ -8,38 +7,35 @@ GAME_TITLE = "Ada or Potato"
 GAME_SPEED = 1/60
 TIMER_MAXIMUM = 100
 
-NEXT_PHASE = {
-    'spinning forward': 'waiting',
-    'waiting': 'spinning backward',
-    'spinning backward': 'waiting again',
-    'waiting again': 'spinning forward'
-    }
-
 IMAGE_ADA = arcade.load_texture("images/ada.png")
-IMAGE_POTATO = arcade.load_texture("images/potato.png")
+IMAGE_POTATO = arcade.load_texture("images/potato.png", scale = .2)
 
+SCORE = 0
 class AdaOrPotato(arcade.Window):
     def __init__(self):
         super().__init__(WINDOW_WIDTH, WINDOW_HEIGHT, GAME_TITLE)
         self.logo_list = None
+        self.score = 0
 
     def setup(self):
         arcade.set_background_color(BACKGROUND_COLOR)
+        self.score = 0
         self.logo_list = arcade.SpriteList()
         self.logo_list.append(ChangingLogo())
-        potato = ChangingLogo()
-        potato.phase = 'spinning backward'
-        potato.center_x = 50
-        potato.center_y = 50
-        self.logo_list.append(potato)
 
     def on_draw(self):
         arcade.start_render()
         self.logo_list.draw()
+        output = f"Score: {self.score}"
+        arcade.draw_text(output, 10, 70, arcade.color.WHITE, 13)
+
+    def on_update(self, delta_time: float):
+        self.logo_list.update()
 
     def on_mouse_press(self, x, y, button, modifiers):
         for logo in self.logo_list:
-            logo.switch_image()
+            if button == arcade.MOUSE_BUTTON_LEFT:
+                self.score += logo.get_current_value()
 
 class ChangingLogo(arcade.Sprite):
     phase: str
@@ -53,31 +49,23 @@ class ChangingLogo(arcade.Sprite):
         self.center_y = WINDOW_HEIGHT/2
         self.texture = IMAGE_ADA
 
-    def update_timer(self):
-        if self.timer < TIMER_MAXIMUM:
-                self.timer += 1
-        else:
-            self.timer = 0
-            self.phase = NEXT_PHASE[self.phase]
-
-    def update_angle(self):
-        progress = self.timer / TIMER_MAXIMUM
-        if self.phase == 'spinning forward':
-            self.angle = 360 * progress
-        elif self.phase == 'spinning backward':
-            self.angle = 360 * (1 - progress)
-        else:
-            self.angle = random.randint(0, 360)
-
     def update(self):
-        self.update_timer()
-        self.update_angle()
+        self.timer += 1
+        if self.timer > TIMER_MAXIMUM:
+            self.timer = 0
+            self.switch_image()
 
     def switch_image(self):
         if self.texture == IMAGE_POTATO:
             self.texture = IMAGE_ADA
         else:
             self.texture = IMAGE_POTATO
+
+    def get_current_value(self):
+        if self.texture == IMAGE_ADA:
+            return 1
+        else:
+            return -1
 
 def main():
     window = AdaOrPotato()
