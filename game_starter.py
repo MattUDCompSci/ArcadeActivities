84753f622262af3c8caf9946a8a2af0be1c22bf2
MattUDCompSci <mattweis@udel.edu>
2019-11-29 19:09:46 -0500

    Set up Class for enemies, Zeke's jumping increases as level progresses, levels reset except for floors

--- game_starter.py
+++ game_starter.py
@@ -6,16 +6,16 @@ WINDOW_WIDTH = 1200
 WINDOW_HEIGHT = 700
 BACKGROUND_COLOR = arcade.color.BLACK
 GAME_TITLE = "Feed Zeke"
-GAME_SPEED = 1/60
+GAME_SPEED = 1/80
 
-ZEKESCALING = 0.15
+ZEKE_SCALING = 0.15
 TILE_SCALING = 0.4
 FOOTBALL_SCALING = 0.4
+ZEKE_TACKLED = 0
 
-ZEKERIGHT = arcade.load_texture("images/Zeke_Right.png", mirrored = False, scale = ZEKESCALING)
-ZEKELEFT = arcade.load_texture("images/Zeke_Right.png", mirrored = True, scale = ZEKESCALING)
+ZEKERIGHT = arcade.load_texture("images/Zeke_Right.png", mirrored = False, scale = ZEKE_SCALING)
+ZEKELEFT = arcade.load_texture("images/Zeke_Right.png", mirrored = True, scale = ZEKE_SCALING)
 
-JUMPSPEED = 30
 MOVESPEED = 5
 GRAVITY = 1
 
@@ -32,9 +32,13 @@ class FeedZeke(arcade.Window):
         self.player_list = None
         self.wall_list = None
         self.football_list = None
+        self.defender_list = None
         self.background = None
         self.score = None
         self.level = None
+        self.jump = None
+        self.tackled = None
+        self.defender = None
         self.current_state = STARTSCREEN
 
         self.instructions = []
@@ -49,14 +53,20 @@ class FeedZeke(arcade.Window):
         self.player_list = arcade.SpriteList()
         self.wall_list = arcade.SpriteList()
         self.football_list = arcade.SpriteList()
+        self.defender_list = arcade.SpriteList()
         self.background = arcade.load_texture("images/Cowboys_Stadium.jpeg")
         self.score = 0
         self.level = 1
+        self.jump = 15
+        self.tackled = False
 
         self.player_sprite = Zeke_Player()
         self.player_sprite.center_x = 64
         self.player_sprite.center_y = 150
         self.player_list.append(self.player_sprite)
+
+        self.defender = Defender_Class()
+        self.defender_list.append(self.defender)
 
         self.level_updater()
 
@@ -78,6 +88,9 @@ class FeedZeke(arcade.Window):
 
         output = f"Score: {self.score}"
         arcade.draw_text(output, 10, 70, arcade.color.WHITE, 20)
+
+        if self.level == 4:
+            self.defender_list.draw()
 
     def on_draw(self):
         arcade.start_render()
@@ -107,18 +120,22 @@ class FeedZeke(arcade.Window):
 
             for football in football_hit_list:
                 football.remove_from_sprite_lists()
-                self.score += 1
+                self.score += 10
+
+            if self.player_sprite.top < 0:
+                self.score -= 10
 
             if self.player_sprite.center_x >= WINDOW_WIDTH:
                 self.level += 1
+                self.jump += 1
                 self.level_updater()
                 self.player_sprite.center_x = 64
-                self.player_sprite.center_y = 150
+                self.player_sprite.center_y = self.player_sprite.center_y
 
     def on_key_press(self, key, modifiers):
         if key == arcade.key.UP or key == arcade.key.W:
             if self.physics_engine.can_jump():
-                self.player_sprite.change_y = JUMPSPEED
+                self.player_sprite.change_y = self.jump
         elif key == arcade.key.LEFT or key == arcade.key.A:
             self.player_sprite.change_x = -MOVESPEED
         elif key == arcade.key.RIGHT or key == arcade.key.D:
@@ -132,20 +149,21 @@ class FeedZeke(arcade.Window):
             self.player_sprite.change_x = 0
 
     def level_updater(self):
-        map_name = f"map_level_{self.level}.tmx"
-        platforms_layer_name = 'Platforms'
-        footballs_layer_name = 'Football'
+        if self.level < 13:
+            map_name = f"map_level_{self.level}.tmx"
+            platforms_layer_name = 'Platforms'
+            footballs_layer_name = 'Football'
 
-        my_map = arcade.tilemap.read_tmx(map_name)
+            my_map = arcade.tilemap.read_tmx(map_name)
 
-        self.wall_list = arcade.tilemap.process_layer(my_map, platforms_layer_name, TILE_SCALING)
-        self.football_list = arcade.tilemap.process_layer(my_map, footballs_layer_name, TILE_SCALING)
-        self.physics_engine = arcade.PhysicsEnginePlatformer(self.player_sprite, self.wall_list, GRAVITY)
+            self.wall_list = arcade.tilemap.process_layer(my_map, platforms_layer_name, TILE_SCALING)
+            self.football_list = arcade.tilemap.process_layer(my_map, footballs_layer_name, TILE_SCALING)
+            self.physics_engine = arcade.PhysicsEnginePlatformer(self.player_sprite, self.wall_list, GRAVITY)
 
-        if self.level%3 == 0:
-            self.background = arcade.load_texture("images/Cowboys_Office.jpeg")
-        else:
-            self.background = arcade.load_texture("images/Cowboys_Stadium.jpeg")
+            if self.level%3 == 0:
+                self.background = arcade.load_texture("images/Cowboys_Office.jpeg")
+            else:
+                self.background = arcade.load_texture("images/Cowboys_Stadium.jpeg")
 
 class Zeke_Player(arcade.Sprite):
 
@@ -164,14 +182,29 @@ class Zeke_Player(arcade.Sprite):
 
         if self.left < 0:
             self.left = 0
-        elif self.right > WINDOW_WIDTH - 1:
-            self.right = WINDOW_WIDTH - 1
 
-        if self.bottom < 0:
-            self.bottom = 0
+        if self.top < 0:
+            self.center_x = 64
+            self.center_y = 150
+
         elif self.top > WINDOW_HEIGHT - 1:
             self.top = WINDOW_HEIGHT - 1
 
+class Defender_Class(arcade.Sprite):
+
+    def __init__(self):
+        super().__init__()
+        self.textures.append(ZEKELEFT)
+        self.textures.append(ZEKERIGHT)
+        self.set_texture(0)
+
+    def update(self):
+        if self.level == 4:
+            self.set_texture(ZEKELEFT)
+            self.center_x = 400
+            self.center_y = 150
+        elif self.level == 5:
+            self.set_texture(ZEKERIGHT)
 
 def main():
     window = FeedZeke()
